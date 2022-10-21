5498bccee5d76cc688f05cd6a3a89744ac5ca397
Ankit <ankit.bhojani@mindinventory.com>
2022-10-21 12:19:27 +0530

    added changes

---
+++ src/api/v1/controllers/script.ts
@@ -0,0 +1,67 @@
+import { UserTable } from "../../../model";
+import { HttpMethod } from "../../../types";
+import { Route } from "../../../utility";
+import BaseController from "./base";
+
+class ScriptController extends BaseController {
+  /**
+   * @description This method is for updating screen status by script for migration into production
+   * @param ctx
+   * @returns
+   */
+  @Route({ path: "/update-screen-status-script", method: HttpMethod.GET })
+  public async updateScreenStatusScript(ctx: any) {
+    /**
+     * Screen status 0 ,1 ,2 to 0
+     */
+    const updatedCount1 = await UserTable.updateMany(
+      { screenStatus: { $in: [0, 1, 2] } },
+      {
+        $set: {
+          screenStatus: 0,
+        },
+      }
+    );
+    /**
+     * Screen status 5 to 7
+     */
+    const updatedCount2 = await UserTable.updateMany(
+      { screenStatus: 5 },
+      {
+        $set: {
+          screenStatus: 7,
+        },
+      }
+    );
+    /**
+     * Screen status 3 to 5
+     */
+    const updatedCount3 = await UserTable.updateMany(
+      { screenStatus: 3 },
+      {
+        $set: {
+          screenStatus: 5,
+        },
+      }
+    );
+    /**
+     * Screen status 4 to 6
+     */
+    const updatedCount4 = await UserTable.updateMany(
+      { screenStatus: 4 },
+      {
+        $set: {
+          screenStatus: 6,
+        },
+      }
+    );
+    return this.Ok(ctx, {
+      updatedCount1,
+      updatedCount2,
+      updatedCount3,
+      updatedCount4,
+    });
+  }
+}
+
+export default new ScriptController();
