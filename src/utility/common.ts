7d2a98a48ac1d0946464a53f673dba73b019187b
Ankit Bhojani <ankit.bhojani@mindinventory.com>
2022-11-22 12:13:32 +0530

    modularization for in-app referral

--- src/utility/common.ts
+++ src/utility/common.ts
@@ -91,6 +91,7 @@ export const makeUniqueReferalCode = async (length = 7) => {
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const charactersLength = characters.length;
     for (let i = 0; i < length; i += 1) {
+      
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
 
