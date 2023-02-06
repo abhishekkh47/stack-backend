fb29c1fcaaec4039a230e012f3c44f632700dccd
Ankit Bhojani <ankit.bhojani@mindinventory.com>
2023-02-06 19:07:58 +0530

    feat: quiz result add api done

--- src/controllers/v3/index.ts
+++ src/controllers/v3/index.ts
@@ -1,7 +1,8 @@
 import AuthController from "./auth";
+import QuizController from "./quiz";
 
 import { getRouteDict } from "../../utility";
 
-const routeDict = getRouteDict("3.0.0", [AuthController]);
+const routeDict = getRouteDict("3.0.0", [AuthController, QuizController]);
 
 export default routeDict;
