e4bcb8dc8051c64a9f88897d97aa8dce09e376d1
stackerbrian <brian@trystack.io>
2023-09-14 15:14:56 -0400

    fix: quicken check-user-signup by not awaiting

--- src/controllers/v4/auth.ts
+++ src/controllers/v4/auth.ts
@@ -542,8 +542,8 @@ class AuthController extends BaseController {
 
               accountCreated = true;
 
-              // for sensitive identify calls,we need to await to make sure it waits.
-              await AnalyticsService.identifyOnce(userExists._id, {
+              // for sensitive identify calls, we need to await to make sure it waits.
+              AnalyticsService.identifyOnce(userExists._id, {
                 Email: userExists.email,
               });
 
