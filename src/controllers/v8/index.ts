74b9c28eeea49ff0fdf70897fd241c12b551810a
Abhishek Khandelwal <abhishek.khandelwal@mindinventory.com>
2024-01-30 14:48:22 +0530

    fix API version in index file

--- src/controllers/v8/index.ts
+++ src/controllers/v8/index.ts
@@ -1,5 +1,5 @@
 import QuizController from "@controllers/v8/quiz";
 import { getRouteDict } from "@app/utility";
-const routeDict = getRouteDict("7.0.0", [QuizController]);
+const routeDict = getRouteDict("8.0.0", [QuizController]);
 
 export default routeDict;
