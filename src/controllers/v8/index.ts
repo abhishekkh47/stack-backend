9ea2a3c859d2a373105ded1950b5c2cffa587231
Abhishek Khandelwal <abhishek.khandelwal@mindinventory.com>
2024-02-15 12:45:14 +0530

    remove reward and add simulation for day-7

--- src/controllers/v8/index.ts
+++ src/controllers/v8/index.ts
@@ -1,5 +1,9 @@
 import QuizController from "@controllers/v8/quiz";
+import WeeklyJourneyController from "@controllers/v8/weekly-journey";
 import { getRouteDict } from "@app/utility";
-const routeDict = getRouteDict("8.0.0", [QuizController]);
+const routeDict = getRouteDict("8.0.0", [
+  QuizController,
+  WeeklyJourneyController,
+]);
 
 export default routeDict;
---
+++ src/services/v8/index.ts
@@ -0,0 +1,1 @@
+export { default as WeeklyJourneyDBService } from "@app/services/v8/weekly-journey.db.service";
