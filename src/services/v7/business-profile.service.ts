8478f27b21ae131f26150a914f7fb642074c066b
NatalieM11 <natalie@xpathmedia.com>
2024-02-23 05:58:01 -0800

    updated pro-tip

--- src/services/v7/business-profile.service.ts
+++ src/services/v7/business-profile.service.ts
@@ -231,7 +231,7 @@ class BusinessProfileService {
               type: "text",
               value: "$valueCreators",
               title: "Top 3 Value Creators",
-              description: "Pro-tip: make each value creator than three words",
+              description: "Pro-tip: make each value creator less than three words",
               placeHolderText: "Enter description...",
               isMultiLine: true,
               maxCharLimit: 280,
