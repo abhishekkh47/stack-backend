901ab5d948b2f05f162e6f9c94366935a2bb181b
Abhishek Khandelwal <abhishek.khandelwal@mindinventory.com>
2024-05-07 11:25:12 +0530

    Update business plan response structure

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -660,8 +660,10 @@ export const BUSINESS_DESCRIPTION_OBJ = {
   actionTitle: "Business Idea",
   hoursSaved: HOURS_SAVED_BY_IDEA_GENERATOR,
   isMultiLine: true,
+  maxCharLimit: 280,
   placeHolderText: "Add Business Idea",
   steps: [],
   title: "Creating Your Perfect Business",
-  actionInput: "Text",
+  type: "text",
+  value: null,
 };
