30d91e561e99413586c0d3a84be1866dcc13f398
Abhishek Khandelwal <abhishek.khandelwal@mindinventory.com>
2024-05-07 13:05:29 +0530

    add topic order in the checklist response

--- src/services/v9/checklist-journey.db.service.ts
+++ src/services/v9/checklist-journey.db.service.ts
@@ -224,6 +224,7 @@ class ChecklistDBService {
         currentActionNum,
         currentLevel,
         topicId: quizCategoryDetails.topicId._id,
+        topicOrder: quizCategoryDetails.topicId.order,
         topic: quizCategoryDetails.topicId.topic,
         categoryId: quizCategoryDetails._id,
         category: quizCategoryDetails.title,
