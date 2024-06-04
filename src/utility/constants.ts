432edea6ae810147a3c7b328a86bd02e98a12d75
Abhishek Khandelwal <abhishek.khandelwal@mindinventory.com>
2024-06-04 10:41:24 +0530

    Fix - Error message for invalid business description

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -430,7 +430,7 @@ export const SUGGESTION_FORMAT = {
 };
 
 export const INVALID_DESCRIPTION_ERROR =
-  "Oops! Looks like we're missing a valid business description to proceed. No worries, simply add it to your profile, and give it another go. Thanks!";
+  "Oops! It seems like the business idea you entered is invalid. Please check your input and try again.";
 export const MAXIMIZE_BUSINESS_IMAGES = [
   "maximize_1.png",
   "maximize_2.png",
