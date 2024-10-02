2601eccf7d2b2deeb6d330cb94c4985a4fbd26c3
Abhishek Khandelwal <abhishek.khandelwal@mindinventory.com>
2024-10-02 11:08:44 +0530

    Fix - Error for old users in milestone progress component

--- src/services/v9/milestone.db.service.ts
+++ src/services/v9/milestone.db.service.ts
@@ -731,23 +731,21 @@ class MilestoneDBService {
         DailyChallengeTable.findOne({ userId }),
       ]);
 
-      let currentGoalId = null;
+      let currentGoalKey = null;
       const dailyGoalStatus = currentGoals?.dailyGoalStatus || [];
       if (dailyGoalStatus?.length > 0) {
-        currentGoalId = new ObjectId(
-          dailyGoalStatus[dailyGoalStatus.length - 1]._id
-        );
+        currentGoalKey = dailyGoalStatus[dailyGoalStatus.length - 1].key;
       } else {
-        currentGoalId = lastGoalCompleted?.goalId;
+        currentGoalKey = lastGoalCompleted?.key;
       }
-      if (!currentGoalId && currentMilestoneId) {
+      if (!currentGoalKey && currentMilestoneId) {
         dailyGoal = await MilestoneGoalsTable.findOne({
           milestoneId: currentMilestoneId,
           day: 1,
         });
       } else {
         dailyGoal = await MilestoneGoalsTable.findOne({
-          _id: currentGoalId,
+          key: currentGoalKey,
         });
       }
 
